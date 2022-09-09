0f7a011a5dbed9ef545cd5aeb0f31a136ec8ea5c
ElviraBaron <elvirabaron@gmail.com>
2022-09-08 20:06:15 -0500

    Prueba cambio tipo de letra

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,8 +1,8 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
         Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
+        miVertebrados[0] = new Vertebrado("CAMELLO", "Mamifero"); 
+        miVertebrados[1] = new Vertebrado("VACA", "Mamifero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
         
         return miVertebrados;
